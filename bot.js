dc9c1005d826456b7845c1ac2a799aa6011b3439
ZueiraIsBae <47709479+ZueiraIsBae@users.noreply.github.com>
2019-02-17 15:08:32 +0700

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,19 @@
+/*
+ * Discord Bot Zueira The Maid
+ * Version 0.9
+ * Zueira
+ */
+
+const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
+
+if(!process.send) {
+
+Files.initStandalone();
+
+} else {
+
+process.on('message', function(content) {
+	Files.initBotTest(content);
+});
+
+}
